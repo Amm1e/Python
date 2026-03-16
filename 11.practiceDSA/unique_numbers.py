536ed61d385f3cb184ef7e81d0edfd3bc1da02fe
Amm1e <amritanshrawat030301@gmail.com>
2026-03-16 21:25:05 +0530

    Added DSA practice problems

---
+++ 11.practiceDSA/unique_numbers.py
@@ -0,0 +1,18 @@
+numbers = [1, 2, 3, 2, 4, 1, 5, 3]
+
+# Task 1: Print unique numbers using a set
+# Task 2: Print how many times each number appeared
+
+set1 =set(numbers)
+print(set1)
+d1={}
+i=1
+for number in numbers:
+    if number in d1.keys():
+        d1[number]+=1
+    else:
+        d1[number]=1
+print(d1)
+
+
+
